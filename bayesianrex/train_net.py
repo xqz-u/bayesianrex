4c293c271642f40cf3b118ea5d5f0983dde4ccf7
xqz-u <marco.gallo0530@gmail.com>
2023-05-21 13:14:22 +0200

    fix for david

--- bayesianrex/train_net.py
+++ bayesianrex/train_net.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+from pathlib import Path
 
 import numpy as np
 import torch
@@ -8,10 +10,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
 
-import argparse
-from pathlib import Path
-
-from bayesianrex.dataset.generate_demonstrations import create_training_data, generate_demonstrations
+from bayesianrex.dataset.generate_demonstrations import (
+    create_training_data,
+    generate_demonstrations,
+)
 
 
 class Net(nn.Module):
@@ -21,31 +23,43 @@ class Net(nn.Module):
         intermediate_dimension = min(784, max(64, ENCODING_DIMS * 2))
         act_fn = nn.LeakyReLU()
         self.pre_reshape = nn.Sequential(
-            nn.Conv2d(4, 16, 7, stride=3), act_fn,
-            nn.Conv2d(16, 32, 5, stride=2), act_fn,
-            nn.Conv2d(32, 32, 3, stride=1), act_fn,
-            nn.Conv2d(32, 16, 3, stride=1), act_fn
+            nn.Conv2d(4, 16, 7, stride=3),
+            act_fn,
+            nn.Conv2d(16, 32, 5, stride=2),
+            act_fn,
+            nn.Conv2d(32, 32, 3, stride=1),
+            act_fn,
+            nn.Conv2d(32, 16, 3, stride=1),
+            act_fn,
         )
         self.post_reshape = nn.Sequential(
             nn.Linear(784, intermediate_dimension), act_fn
         )
         self.decoder_pre_reshape = nn.Sequential(
-            nn.Linear(ENCODING_DIMS, intermediate_dimension), act_fn,
-            nn.Linear(intermediate_dimension, 1568), act_fn
+            nn.Linear(ENCODING_DIMS, intermediate_dimension),
+            act_fn,
+            nn.Linear(intermediate_dimension, 1568),
+            act_fn,
         )
         self.decoder_post_reshape = nn.Sequential(
-            nn.ConvTranspose2d(2, 4, 3, stride=1), act_fn,
-            nn.ConvTranspose2d(4, 16, 6, stride=1), act_fn,
-            nn.ConvTranspose2d(16, 16, 7, stride=2), act_fn,
-            nn.ConvTranspose2d(16, 4, 10, stride=1), nn.Sigmoid()
+            nn.ConvTranspose2d(2, 4, 3, stride=1),
+            act_fn,
+            nn.ConvTranspose2d(4, 16, 6, stride=1),
+            act_fn,
+            nn.ConvTranspose2d(16, 16, 7, stride=2),
+            act_fn,
+            nn.ConvTranspose2d(16, 4, 10, stride=1),
+            nn.Sigmoid(),
         )
         self.fc_mu = nn.Linear(intermediate_dimension, ENCODING_DIMS)
         self.fc_var = nn.Linear(intermediate_dimension, ENCODING_DIMS)
         self.fc1 = nn.Linear(ENCODING_DIMS, 1)
 
         self.temporal_difference1 = nn.Linear(ENCODING_DIMS * 2, 1, bias=False)
-        self.inverse_dynamics1 = nn.Linear(ENCODING_DIMS * 2, ACTION_DIMS, bias=False)  
-        self.forward_dynamics1 = nn.Linear(ENCODING_DIMS + ACTION_DIMS, ENCODING_DIMS, bias=False)
+        self.inverse_dynamics1 = nn.Linear(ENCODING_DIMS * 2, ACTION_DIMS, bias=False)
+        self.forward_dynamics1 = nn.Linear(
+            ENCODING_DIMS + ACTION_DIMS, ENCODING_DIMS, bias=False
+        )
 
         self.normal = tdist.Normal(0, 1)
         self.sigmoid = nn.Sigmoid()
@@ -108,9 +122,12 @@ class Net(nn.Module):
         return (
             torch.cat((cum_r_i.unsqueeze(0), cum_r_j.unsqueeze(0)), 0),
             abs_r_i + abs_r_j,
-            z1, z2,
-            mu1, mu2,
-            var1, var2,
+            z1,
+            z2,
+            mu1,
+            mu2,
+            var1,
+            var2,
         )
 
 
@@ -125,6 +142,7 @@ def reconstruction_loss(decoded, target, mu, logvar):
     # print("bce: " + str(bce) + " kld: " + str(kld))
     return bce + kld
 
+
 def compute_losses(reward_network, z, mu, logvar, traj, times, num_frames, actions_):
     inverse_dynamics_loss_fn = nn.CrossEntropyLoss()
     forward_dynamics_loss_fn = nn.MSELoss()
@@ -132,22 +150,24 @@ def compute_losses(reward_network, z, mu, logvar, traj, times, num_frames, actio
     decoded = reward_network.decode(z)
     recon_loss = 10 * reconstruction_loss(decoded, traj, mu, logvar)
     t1, t2 = tuple(np.random.randint(low=0, high=len(times), size=2))
-    est_dt = reward_network.estimate_temporal_difference(mu[t1].unsqueeze(0), mu[t2].unsqueeze(0))
+    est_dt = reward_network.estimate_temporal_difference(
+        mu[t1].unsqueeze(0), mu[t2].unsqueeze(0)
+    )
     real_dt = (times[t2] - times[t1]) / 100.0
 
     actions = reward_network.estimate_inverse_dynamics(mu[0:-1], mu[1:])
     target_actions = torch.LongTensor(actions_[1:]).to(device)
 
-    inverse_dynamics_loss = (inverse_dynamics_loss_fn(actions, target_actions) / 1.9)
+    inverse_dynamics_loss = inverse_dynamics_loss_fn(actions, target_actions) / 1.9
     forward_dynamics_distance = 5
-    forward_dynamics_onehot_actions_1 = torch.zeros((num_frames - 1, reward_network.ACTION_DIMS), dtype=torch.float32, device=device)
+    forward_dynamics_onehot_actions_1 = torch.zeros(
+        (num_frames - 1, reward_network.ACTION_DIMS), dtype=torch.float32, device=device
+    )
     forward_dynamics_onehot_actions_1.scatter_(1, target_actions.unsqueeze(1), 1.0)
 
     forward_dynamics = reward_network.forward_dynamics(
         mu[:-forward_dynamics_distance],
-        forward_dynamics_onehot_actions_1[
-        : (num_frames - forward_dynamics_distance)
-        ],
+        forward_dynamics_onehot_actions_1[: (num_frames - forward_dynamics_distance)],
     )
 
     for fd_i in range(forward_dynamics_distance - 1):
@@ -158,24 +178,29 @@ def compute_losses(reward_network, z, mu, logvar, traj, times, num_frames, actio
             ],
         )
 
-    forward_dynamics_loss = 100 * forward_dynamics_loss_fn(forward_dynamics, mu[forward_dynamics_distance:])
-    dt_loss_i = 4 * temporal_difference_loss(est_dt, torch.tensor(((real_dt,),), dtype=torch.float32, device=device))
+    forward_dynamics_loss = 100 * forward_dynamics_loss_fn(
+        forward_dynamics, mu[forward_dynamics_distance:]
+    )
+    dt_loss_i = 4 * temporal_difference_loss(
+        est_dt, torch.tensor(((real_dt,),), dtype=torch.float32, device=device)
+    )
 
     return sum([dt_loss_i, forward_dynamics_loss, recon_loss, inverse_dynamics_loss])
 
+
 # Train the network
 def learn_reward(
-        reward_network,
-        optimizer,
-        inputs,
-        outputs,
-        actions,
-        times,
-        num_iter,
-        l1_reg,
-        loss_fn,
-        checkpoint_dir="../model_checkpoints"
-    ):
+    reward_network,
+    optimizer,
+    inputs,
+    outputs,
+    actions,
+    times,
+    num_iter,
+    l1_reg,
+    loss_fn,
+    checkpoint_dir="../model_checkpoints",
+):
     os.makedirs(checkpoint_dir, exist_ok=True)
     print(f"Saving outputs to {os.path.abspath(checkpoint_dir)}")
     # check if gpu available
@@ -199,7 +224,9 @@ def learn_reward(
             actions_i, actions_j = training_actions_sub[i]
 
             traj_i, traj_j = np.array(traj_i), np.array(traj_j)
-            traj_i, traj_j = torch.from_numpy(traj_i).float().to(device), torch.from_numpy(traj_j).float().to(device)
+            traj_i, traj_j = torch.from_numpy(traj_i).float().to(
+                device
+            ), torch.from_numpy(traj_j).float().to(device)
             labels = torch.from_numpy(labels).to(device)
             num_frames_i, num_frames_j = len(traj_i), len(traj_j)
 
@@ -207,14 +234,41 @@ def learn_reward(
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs, abs_rewards, z1, z2, mu1, mu2, logvar1, logvar2 = reward_network.forward(traj_i, traj_j)
+            (
+                outputs,
+                abs_rewards,
+                z1,
+                z2,
+                mu1,
+                mu2,
+                logvar1,
+                logvar2,
+            ) = reward_network.forward(traj_i, traj_j)
             outputs = outputs.unsqueeze(0)
 
             # compute losses
-            losses_i = compute_losses(reward_network, z1, mu1, logvar1, traj_i, times_i, num_frames_i, actions_i)
-            losses_j = compute_losses(reward_network, z2, mu2, logvar2, traj_j, times_j, num_frames_j, actions_j)
+            losses_i = compute_losses(
+                reward_network,
+                z1,
+                mu1,
+                logvar1,
+                traj_i,
+                times_i,
+                num_frames_i,
+                actions_i,
+            )
+            losses_j = compute_losses(
+                reward_network,
+                z2,
+                mu2,
+                logvar2,
+                traj_j,
+                times_j,
+                num_frames_j,
+                actions_j,
+            )
             trex_loss = loss_criterion(outputs, labels.long())
-            
+
             if loss_fn == "trex":  # only use trex loss
                 loss = trex_loss
             elif loss_fn == "ss":  # only use self-supervised loss
@@ -262,7 +316,9 @@ def calc_accuracy(reward_network, training_inputs, training_outputs):
             label = training_outputs[i]
             traj_i, traj_j = training_inputs[i]
             traj_i, traj_j = np.array(traj_i), np.array(traj_j)
-            traj_i, traj_j = torch.from_numpy(traj_i).float().to(device), torch.from_numpy(traj_j).float().to(device)
+            traj_i, traj_j = torch.from_numpy(traj_i).float().to(
+                device
+            ), torch.from_numpy(traj_j).float().to(device)
 
             # forward to get logits
             outputs, _, _, _, _, _, _, _ = reward_network.forward(traj_i, traj_j)
@@ -274,43 +330,77 @@ def calc_accuracy(reward_network, training_inputs, training_outputs):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('--env_name', default='breakout', help='Select the environment name to run, i.e. pong')
-    parser.add_argument('--reward_model_path', default='./reward_model_checkpoints/', help="name and location for learned model params, e.g. ./learned_models/breakout.params")
-    parser.add_argument('--seed', default=0, help="random seed for experiments")
-    parser.add_argument('--models_dir', default = ".", help="path to directory that contains a models directory in which the checkpoint models for demos are stored")
-    parser.add_argument('--encoding_dims', default = 64, type = int, help = "number of dimensions in the latent space")
-    parser.add_argument('--loss_fn', default='trex+ss', help="ss: selfsupervised, trex: only trex, trex+ss: both trex and selfsupervised")
-    parser.add_argument('--training_data_available', default=False, help="If training data not availabel we generate it locally")
-
-    parser.add_argument('--data_seed', default=0, type=int, help="RNG seed")
-    parser.add_argument('--checkpoints_dir', type=Path,
-            help=
-                """path to a directory containing PPO checkpoints, e.g."""
-                """ 'data/demonstrations/BreakoutNoFrameskip-v4'""")
-    parser.add_argument('--env', type=str,
-            default="breakout",
-            help=(
-                """name of Atari game to generate demonstrations (should be"""
-                """ same as checkpoints training env)"""
-            ),)
-    parser.add_argument('--n_episodes', type=int,
-            default=1,
-            help="number of episodes to play (1 episode = 1 full trajectory)")
-    parser.add_argument('--n-traj', type=int,
-            default=0,
-            help="number of full training demonstrations") 
-    parser.add_argument('--n-snippets', type=int,
-            default=int(6e4),
-            help="number of short training demonstrations") 
-    parser.add_argument('--snippet-min-len', type=int,
-            default=50,
-            help="minimum length of short demonstrations") 
-    parser.add_argument('--snippet-max-len', type=int,
-            default=100,
-            help="max length of short demonstrations") 
-    parser.add_argument('--n-envs', type=int,
-            default=1,
-            help="number of envs to run in parallel to gather demonstrations") 
+    # NOTE why this argument here? we learn those weights in this file
+    parser.add_argument(
+        "--reward_model_path",
+        default="./reward_model_checkpoints/",
+        help="name and location for learned model params, e.g. ./learned_models/breakout.params",
+    )
+    parser.add_argument("--seed", default=0, help="random seed for experiments")
+    parser.add_argument(
+        "--encoding_dims",
+        default=64,
+        type=int,
+        help="number of dimensions in the latent space",
+    )
+    parser.add_argument(
+        "--loss_fn",
+        default="trex+ss",
+        help="ss: selfsupervised, trex: only trex, trex+ss: both trex and selfsupervised",
+    )
+    parser.add_argument(
+        "--training_data_available",
+        default=False,
+        help="If training data not availabel we generate it locally",
+    )
+    parser.add_argument(
+        "--checkpoints_dir",
+        type=Path,
+        help="""path to a directory containing PPO checkpoints, e.g."""
+        """ 'data/demonstrations/BreakoutNoFrameskip-v4'""",
+    )
+    parser.add_argument(
+        "--env",
+        type=str,
+        default="breakout",
+        help=(
+            """name of Atari game to generate demonstrations (should be"""
+            """ same as checkpoints training env)"""
+        ),
+    )
+    parser.add_argument(
+        "--n_episodes",
+        type=int,
+        default=1,
+        help="number of episodes to play (1 episode = 1 full trajectory)",
+    )
+    parser.add_argument(
+        "--n-traj", type=int, default=0, help="number of full training demonstrations"
+    )
+    parser.add_argument(
+        "--n-snippets",
+        type=int,
+        default=int(6e4),
+        help="number of short training demonstrations",
+    )
+    parser.add_argument(
+        "--snippet-min-len",
+        type=int,
+        default=50,
+        help="minimum length of short demonstrations",
+    )
+    parser.add_argument(
+        "--snippet-max-len",
+        type=int,
+        default=100,
+        help="max length of short demonstrations",
+    )
+    parser.add_argument(
+        "--n-envs",
+        type=int,
+        default=1,
+        help="number of envs to run in parallel to gather demonstrations",
+    )
 
     args = parser.parse_args()
 
@@ -318,10 +408,9 @@ if __name__ == "__main__":
     encoding_dims = 64
     ACTION_DIMS = 4
     l1_reg = 0.0
-    num_iter = 2 if args.env_name == "enduro" and args.loss_fn == "trex+ss" else 1
+    num_iter = 2 if args.env == "enduro" and args.loss_fn == "trex+ss" else 1
 
     if args.training_data_available:
-         
         # load training data from checkpoints
         folder = "../training_data"
         obs = np.load(f"{folder}/training_obs.npy", allow_pickle=True)
@@ -331,31 +420,36 @@ if __name__ == "__main__":
         demonstrations = np.load(f"{folder}/demonstrations.npy", allow_pickle=True)
         sorted_returns = np.load(f"{folder}/training_obs.npy", allow_pickle=True)
     else:
-
         # generate training data
         # checkpoints = args.checkpoints_dir
+        from bayesianrex import config
+
         checkpoints = [
-            Path("agent_checkpoints/BreakoutNoFrameskip-v4/PPO_9950_steps.zip"),
-            Path("agent_checkpoints/BreakoutNoFrameskip-v4/PPO_3100_steps.zip"),
+            # Path("agent_checkpoints/BreakoutNoFrameskip-v4/PPO_9950_steps.zip"),
+            # Path("agent_checkpoints/BreakoutNoFrameskip-v4/PPO_3100_steps.zip"),
+            Path(config.DEMONSTRATIONS_DIR / "BreakoutNoFrameskip-v4/PPO_50_steps.zip"),
+            Path(
+                config.DEMONSTRATIONS_DIR / "BreakoutNoFrameskip-v4/PPO_9950_steps.zip"
+            ),
         ]
         trajectories = generate_demonstrations(
-        checkpoints,
-        args.env,
-        args.n_episodes,
-        seed=args.data_seed,
-        n_envs=args.n_envs,
+            checkpoints,
+            args.env,
+            args.n_episodes,
+            seed=args.seed,
+            n_envs=args.n_envs,
         )
         training_data = create_training_data(
             trajectories,
             args.n_traj,
             args.n_snippets,
-            np.random.default_rng(args.data_seed),
+            np.random.default_rng(args.seed),
             (args.snippet_min_len, args.snippet_max_len),
         )
         obs, actions, times, labels = training_data
         demonstrations = trajectories
         sorted_returns = obs
-        print(f'Generated data')
+        print(f"Generated data")
 
     # Now we create a reward network and optimize it using the training data.
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -363,13 +457,25 @@ if __name__ == "__main__":
     reward_net.to(device)
 
     optimizer = optim.Adam(reward_net.parameters(), lr=lr, weight_decay=weight_decay)
-    learn_reward(reward_net, optimizer, obs, labels, actions, times, num_iter, l1_reg, args.loss_fn,)
+    learn_reward(
+        reward_net,
+        optimizer,
+        obs,
+        labels,
+        actions,
+        times,
+        num_iter,
+        l1_reg,
+        args.loss_fn,
+    )
     # save reward network
     torch.save(reward_net.state_dict(), args.reward_model_path)
 
     # print out predicted cumulative returns and actual returns
     with torch.no_grad():
-        pred_returns = [sum(predict_reward_sequence(reward_net, traj[0])) for traj in demonstrations]
+        pred_returns = [
+            sum(predict_reward_sequence(reward_net, traj[0])) for traj in demonstrations
+        ]
     for i, p in enumerate(pred_returns):
         print(i, p, sorted_returns[i])
     print("accuracy", calc_accuracy(reward_net, obs, labels))
