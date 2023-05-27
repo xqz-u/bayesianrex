8818028011049299e70bcc08952e17214944811d
xqz-u <marco.gallo0530@gmail.com>
2023-05-27 13:40:00 +0200

    merging into master and removing original codebase - our fork is independent

--- bayesianrex/DemoGraphRunner.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-fd = os.listdir(sys.argv[1])
-for nname in fd:
-    if " " in nname:
-        print("Skipping " + str(nname) + " due to space")
-        continue
-    os.system("python DemoGraph.py " + sys.argv[1] + " " + str(nname))
