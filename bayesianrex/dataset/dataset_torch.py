8703037e8b59684f30dc0fb98fafebcaf051a111
Satchit Chatterji <satchit.chatterji@gmail.com>
2023-05-28 15:03:29 +0200

    Finished docstring

--- bayesianrex/dataset/dataset_torch.py
+++ bayesianrex/dataset/dataset_torch.py
@@ -10,17 +10,27 @@ DemonstrationDatapoint = Union[Tuple[T, T], T]
 
 
 class RLDemonstrationsDataset(Dataset):
+    """
+    Class to store and handle demonstration data
+    """
     datapoints: List[DemonstrationDatapoint]
 
     def __init__(self, demonstrations: TrainTrajectories):
+        """
+        Initialize the RLDemonstrationsDataset.
+        :param demonstrations: Training trajectories consisting of states, actions, and times
+        """
         self.datapoints = tensorify(list(zip(*demonstrations)))
 
     def __len__(self) -> int:
+        """ Return length of dataset """
         return len(self.datapoints)
 
     def __getitem__(self, idx: int) -> List[DemonstrationDatapoint]:
+        """ Return item at index idx at dataset """
         return self.datapoints[idx]
 
 
 def make_demonstrations_loader(data: TrainTrajectories, shuffle=True) -> DataLoader:
+    """ Create a DataLoader for RL demonstrations """
     return DataLoader(RLDemonstrationsDataset(data), shuffle=shuffle, batch_size=1)
